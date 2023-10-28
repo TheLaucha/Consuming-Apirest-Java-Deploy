9d80967893338d4d43d95cf528b480167eb2be85
Micaela1m <micaela_a_m@outlook.com>
2023-10-28 20:20:49 -0300

    Se realiza cambio a POST

--- src/main/java/com/ideas/springboot/web/app/controllers/TeamController.java
+++ src/main/java/com/ideas/springboot/web/app/controllers/TeamController.java
@@ -38,10 +38,8 @@ public class TeamController {
       */
 
     //@CrossOrigin(origins = "http://localhost:5173") 
-    //@PostMapping("/buscarclima")
-    @GetMapping("/buscarclima")
-    public TeamResponse buscarClima(/*@RequestParam String region*/) {
-        String region = "Cordoba";
+    @PostMapping("/buscarclima")
+    public TeamResponse buscarClima(@RequestParam String region) {
         TeamResponse respuesta = this.teamService.buscarClima(region);
         datosClima.create(respuesta);
         return respuesta;
