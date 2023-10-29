155e5bfdcca0ef78f3b7f920b5433afb5050d5e3
Micaela1m <micaela_a_m@outlook.com>
2023-10-29 13:14:26 -0300

    Cambio de configuracion en CROSS

--- src/main/java/com/ideas/springboot/web/app/SpringBootWebApplication.java
+++ src/main/java/com/ideas/springboot/web/app/SpringBootWebApplication.java
@@ -3,8 +3,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 @EnableJpaRepositories(basePackages = "com.ideas.springboot.web.app.service.dto.team")
 @EntityScan(basePackages = "com.ideas.springboot.web.app.service.dto.team")
 public class SpringBootWebApplication {
